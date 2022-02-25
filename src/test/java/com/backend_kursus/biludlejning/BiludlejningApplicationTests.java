21f40023ee36be901fd3b279293821af58fbb265
Johannes <joecthomsen@gmail.com>
2022-02-25 13:01:47 +0100

    Added missing jar files

---
+++ src/test/java/com/backend_kursus/biludlejning/BiludlejningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_kursus.biludlejning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiludlejningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
