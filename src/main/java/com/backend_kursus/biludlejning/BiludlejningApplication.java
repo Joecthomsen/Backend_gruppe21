b63e0fe383116816af69dd5fab508a93f33980e7
Johannes <joecthomsen@gmail.com>
2022-02-25 14:12:12 +0100

    test1001

--- src/main/java/com/backend_kursus/biludlejning/BiludlejningApplication.java
+++ src/main/java/com/backend_kursus/biludlejning/BiludlejningApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BiludlejningApplication {
+public class  BiludlejningApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BiludlejningApplication.class, args);
